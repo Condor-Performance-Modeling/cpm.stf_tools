4502fd764c76a237fc572911b73a71022ec86208
Edmund Gieske <edmund.gieske@condorcomputing.com>
2025-07-09 15:59:29 -0500

    Added counts of prefix not-takens and direction changes and sorting by occurences minus prefix not-takens

--- tools/stf_branch_classify/stf_branch_classify.cpp
+++ tools/stf_branch_classify/stf_branch_classify.cpp
@@ -3,6 +3,8 @@
 #include <memory>
 #include <set>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 #include "print_utils.hpp"
 #include "stf_branch_reader.hpp"
@@ -40,10 +42,20 @@ enum class Direction : uint8_t {
 };
 
 struct BranchInfo {
+    uint64_t not_taken_prefix = 0;
     uint64_t taken = 0;
     uint64_t not_taken = 0;
+    uint64_t direction_changes;
+    uint64_t sequential_taken = 0;
+    uint64_t prev_sequential_taken = 0;
+    uint64_t sequential_not_taken = 0;
+    uint64_t max_sequential_taken = 0;
+    uint64_t max_sequential_not_taken = 0;
     std::map<uint64_t, uint64_t> targets;
+    bool previous_taken = false;
+    bool stable_seq_taken = true;
     bool indirect = false;
+    bool repeated = false;
     Direction direction = Direction::INVALID;
 };
 
@@ -53,6 +65,9 @@ int main(int argc, char** argv) {
     bool only_taken = false;
     bool only_dynamic = false;
     bool skip_non_user = false;
+    uint64_t preceding_branch_pc = 0x0;
+    uint64_t all_total = 0;
+    uint64_t running_total = 0;
 
     try {
         processCommandLine(argc, argv, trace, verbose, only_taken, only_dynamic, skip_non_user);
@@ -65,15 +80,52 @@ int main(int argc, char** argv) {
     stf::STFBranchReader reader(trace, skip_non_user);
 
     std::map<uint64_t, BranchInfo> branch_counts;
+    std::vector<std::pair<uint64_t, BranchInfo>> summary;
 
     for(const auto& branch: reader) {
         auto& branch_info = branch_counts[branch.getPC()];
 
         const bool is_taken = branch.isTaken();
+
+        if ((branch_info.taken == 0) && !is_taken) {
+            branch_info.not_taken_prefix++;
+            //continue;
+        }
+        
+        if (((branch_info.taken + branch_info.not_taken) > 0) && (branch_info.previous_taken != is_taken)) {
+            branch_info.direction_changes++;
+
+            if (is_taken) {
+                branch_info.sequential_taken     = 1;
+                branch_info.max_sequential_taken = (branch_info.max_sequential_taken > 1) ? branch_info.max_sequential_taken : 1;
+            }
+            else {
+                if (branch_info.not_taken) {
+                    branch_info.stable_seq_taken = branch_info.stable_seq_taken && (branch_info.sequential_taken == branch_info.prev_sequential_taken);
+                    branch_info.prev_sequential_taken = branch_info.sequential_taken;
+                }
+                branch_info.sequential_not_taken = 1;
+                branch_info.max_sequential_not_taken = (branch_info.max_sequential_not_taken > 1) ? branch_info.max_sequential_not_taken : 1;
+            }
+        }
+        else {
+            if (is_taken) {
+                branch_info.sequential_taken++;
+                branch_info.max_sequential_taken = (branch_info.max_sequential_taken > branch_info.sequential_taken) ? branch_info.max_sequential_taken : branch_info.sequential_taken;
+            }
+            else {
+                branch_info.sequential_not_taken++;
+                branch_info.max_sequential_not_taken = (branch_info.max_sequential_not_taken > branch_info.sequential_not_taken) ? branch_info.max_sequential_not_taken : branch_info.sequential_not_taken;
+            }
+        }
         branch_info.taken += is_taken;
         branch_info.not_taken += !is_taken;
+        branch_info.previous_taken = is_taken;
+        //if (branch_info.not_taken == 0) branch_info.initial_consecutive_takens += is_taken;
         branch_info.targets[branch.getTargetPC()] += is_taken;
         branch_info.indirect = branch.isIndirect();
+        branch_info.repeated = (branch.getPC()==preceding_branch_pc);
+        preceding_branch_pc  = branch.getPC();
 
         Direction new_dir = Direction::FORWARD;
         if(branch.isCall()) {
@@ -92,26 +144,53 @@ int main(int argc, char** argv) {
             stf_assert(branch_info.indirect, "Only indirect branches can have multiple directions");
             branch_info.direction = Direction::MULTIPLE;
         }
+        all_total++;
     }
 
-    static constexpr int COLUMN_WIDTH = 20;
-    stf::print_utils::printLeft("Instruction PC", COLUMN_WIDTH);
+
+    for (const auto& pair : branch_counts) {
+        summary.push_back(pair);
+    }
+    //auto test = summary[0].second.not_taken;
+    //std::cout << "Test before sort: " << test << std::endl;
+    std::sort(summary.begin(), summary.end(), [](const auto& a, const auto& b) {
+        return ((a.second.taken + a.second.not_taken - a.second.not_taken_prefix) > (b.second.taken + b.second.not_taken - b.second.not_taken_prefix)); 
+    });
+    //test = summary[0].second.not_taken;
+    //std::cout << "Test after sort: " << test << std::endl;
+    std::cout << "Total Instances: " << all_total << std::endl;
+    uint64_t limit = int(0.98*(double)all_total);
+    bool limit_reached = false;
+
+    static constexpr int COLUMN_WIDTH = 17;
+    stf::print_utils::printLeft("Rank", 6);
+    stf::print_utils::printLeft("Instr PC", COLUMN_WIDTH);
     stf::print_utils::printLeft("Total Instances", COLUMN_WIDTH);
     stf::print_utils::printLeft("Behavior", COLUMN_WIDTH);
     stf::print_utils::printLeft("Direction", COLUMN_WIDTH);
     stf::print_utils::printLeft("Indirect", COLUMN_WIDTH);
+    stf::print_utils::printLeft("Repeated", COLUMN_WIDTH);    
     stf::print_utils::printLeft("# Targets", COLUMN_WIDTH);
-    stf::print_utils::printLeft("Taken Count", COLUMN_WIDTH);
-    stf::print_utils::printLeft("Not Taken Count", COLUMN_WIDTH);
+    stf::print_utils::printLeft("NT Prefix", COLUMN_WIDTH); 
+    stf::print_utils::printLeft("Takens", COLUMN_WIDTH);
+    stf::print_utils::printLeft("Not Takens", COLUMN_WIDTH);
+    stf::print_utils::printLeft("Dir Changes", COLUMN_WIDTH);
+    //stf::print_utils::printLeft("Stable Seq Tkns", COLUMN_WIDTH);
+    stf::print_utils::printLeft("Max Seq Takens", COLUMN_WIDTH);
+    stf::print_utils::printLeft("Max Seq NotTkns", COLUMN_WIDTH);    
     if(verbose) {
         stf::print_utils::printLeft("Target", COLUMN_WIDTH);
         stf::print_utils::printLeft("Traversals", COLUMN_WIDTH);
     }
     std::cout << std::endl;
-    for(const auto& branch: branch_counts) {
+
+    uint64_t rank = 0;
+
+    for(const auto& branch: summary) {
         const auto& branch_info = branch.second;
         const auto taken = branch_info.taken;
         const auto not_taken = branch_info.not_taken;
+
 
         if(only_dynamic && !(taken && not_taken)) {
             continue;
@@ -123,8 +202,16 @@ int main(int argc, char** argv) {
 
         const auto pc = branch.first;
         const auto total = taken + not_taken;
+        
+        running_total += total;
+        if (running_total>limit && !limit_reached) {
+            std::cout << "---Limit reached---" << std::endl;
+            limit_reached = true;
+        }
+
         stf_assert(total, "Invalid branch behavior for pc " << std::hex << pc);
 
+        stf::print_utils::printDecLeft(++rank, 6);
         stf::print_utils::printHex(pc);
         stf::print_utils::printSpaces(4);
         stf::print_utils::printDecLeft(total, COLUMN_WIDTH);
@@ -141,10 +228,10 @@ int main(int argc, char** argv) {
 
         switch(branch_info.direction) {
             case Direction::FORWARD:
-                stf::print_utils::printLeft("FORWARD", COLUMN_WIDTH);
+                stf::print_utils::printLeft("Fw->", COLUMN_WIDTH);
                 break;
             case Direction::BACKWARD:
-                stf::print_utils::printLeft("BACKWARD", COLUMN_WIDTH);
+                stf::print_utils::printLeft("<-Bk", COLUMN_WIDTH);
                 break;
             case Direction::CALL:
                 stf::print_utils::printLeft("CALL", COLUMN_WIDTH);
@@ -163,13 +250,35 @@ int main(int argc, char** argv) {
             stf::print_utils::printLeft('Y', COLUMN_WIDTH);
         }
         else {
-            stf::print_utils::printLeft('N', COLUMN_WIDTH);
+            stf::print_utils::printLeft(" n", COLUMN_WIDTH);
+        }
+        if(branch_info.repeated) {
+            stf::print_utils::printLeft('Y', COLUMN_WIDTH);
+        }
+        else {
+            stf::print_utils::printLeft(" n", COLUMN_WIDTH);
         }
 
         stf::print_utils::printDecLeft(branch_info.targets.size(), COLUMN_WIDTH);
-
+        stf::print_utils::printDecLeft(branch_info.not_taken_prefix, COLUMN_WIDTH);
         stf::print_utils::printDecLeft(taken, COLUMN_WIDTH);
         stf::print_utils::printDecLeft(not_taken, COLUMN_WIDTH);
+        stf::print_utils::printDecLeft(branch_info.direction_changes, COLUMN_WIDTH);
+        if (taken && not_taken) {
+ /*            if (branch_info.stable_seq_taken) {
+                stf::print_utils::printLeft('Y', COLUMN_WIDTH);
+            }
+            else {
+                stf::print_utils::printLeft('n', COLUMN_WIDTH);
+            } */
+            stf::print_utils::printDecLeft(branch_info.max_sequential_taken, COLUMN_WIDTH);
+            stf::print_utils::printDecLeft(branch_info.max_sequential_not_taken, COLUMN_WIDTH);
+        }
+        else {
+            //stf::print_utils::printLeft("-", COLUMN_WIDTH);
+            stf::print_utils::printLeft("-", COLUMN_WIDTH);
+            stf::print_utils::printLeft("-", COLUMN_WIDTH);
+        }
 
         if(verbose) {
             bool first_line = true;
