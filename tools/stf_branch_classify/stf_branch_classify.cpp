bedba3a9df98799f4c83d0760eafef49201dc5e7
Edmund Gieske <edmund.gieske@condorcomputing.com>
2025-07-11 22:30:29 -0500

    Added numerous features including optional BTB & bimodal index usage.

--- tools/stf_branch_classify/stf_branch_classify.cpp
+++ tools/stf_branch_classify/stf_branch_classify.cpp
@@ -16,18 +16,21 @@ void processCommandLine(int argc,
                         bool& verbose,
                         bool& only_taken,
                         bool& only_dynamic,
-                        bool& skip_non_user) {
+                        bool& skip_non_user,
+                        bool& btb_index) {
     trace_tools::CommandLineParser parser("stf_branch_classify");
     parser.addFlag('v', "verbose mode (prints indirect branch targets)");
     parser.addFlag('t', "only report taken branches (branches that are taken at least once)");
     parser.addFlag('d', "only report dynamic branches (branches that are not always-taken or never-taken)");
     parser.addFlag('u', "skip non user-mode instructions");
+    parser.addFlag('b', "report 1K BTB index allocations & hits");
     parser.addPositionalArgument("trace", "trace in STF format");
     parser.parseArguments(argc, argv);
     verbose = parser.hasArgument('v');
     only_taken = parser.hasArgument('t');
     only_dynamic = parser.hasArgument('d');
     skip_non_user = parser.hasArgument('u');
+    btb_index = parser.hasArgument('b');
 
     parser.getPositionalArgument(0, trace);
 }
@@ -36,9 +39,63 @@ enum class Direction : uint8_t {
     INVALID,
     FORWARD,
     BACKWARD,
-    CALL,
-    RETURN,
     MULTIPLE
+};
+
+enum class BranchType : uint8_t {
+    INVALID,
+    CONDITIONAL,
+    CALL,
+    M_CALL,
+    RETURN,
+    M_RET,
+    JUMP,
+    M_JUMP,
+    ERROR
+};
+
+class BTB {
+    struct CacheInfo {
+        uint64_t tag = 0;
+        uint64_t allocations = 0;
+        uint64_t hits = 0;
+        bool valid = false;
+        std::map<uint64_t,uint64_t> unique_pcs;
+    };
+    std::array<CacheInfo, 1024> table_;
+    uint64_t index_mask_ = (1024-1);
+public:
+    void access(uint64_t addr) {
+        addr >>= 1;
+        uint64_t index = addr & index_mask_;
+        uint64_t atag  = (addr & ~index_mask_);
+        if (!table_[index].valid) {
+            table_[index].tag = atag;
+            table_[index].allocations++;
+            table_[index].valid = true;
+        }
+        else {
+            if (table_[index].tag == atag) table_[index].hits++;
+            else {
+                table_[index].tag = atag;
+                table_[index].allocations++;
+                table_[index].valid = true;
+            }
+        };
+        table_[index].unique_pcs[addr]++;
+    };
+    uint64_t getAllocations(uint64_t index) {
+        index &= index_mask_;
+        return table_[index].allocations;
+    };
+    uint64_t getHits(uint64_t index) {
+        index &= index_mask_;
+        return table_[index].hits;
+    };
+    uint64_t getNumUniquePCs(uint64_t index) {
+        index &= index_mask_;
+        return table_[index].unique_pcs.size();
+    }; 
 };
 
 struct BranchInfo {
@@ -52,11 +109,16 @@ struct BranchInfo {
     uint64_t max_sequential_taken = 0;
     uint64_t max_sequential_not_taken = 0;
     std::map<uint64_t, uint64_t> targets;
+    std::map<uint64_t, uint64_t> precedents;
+    std::map<uint64_t, uint64_t> pre_precedents;
+    std::map<uint64_t, uint64_t> taken_precedents;
+    std::map<uint64_t, uint64_t> indirect_precedents;
     bool previous_taken = false;
     bool stable_seq_taken = true;
     bool indirect = false;
     bool repeated = false;
     Direction direction = Direction::INVALID;
+    BranchType type = BranchType::INVALID;
 };
 
 int main(int argc, char** argv) {
@@ -65,12 +127,19 @@ int main(int argc, char** argv) {
     bool only_taken = false;
     bool only_dynamic = false;
     bool skip_non_user = false;
+    bool btb_index = false;
     uint64_t preceding_branch_pc = 0x0;
+    uint64_t pre_preceding_branch_pc = 0x0;
+    uint64_t preceding_taken_branch_pc = 0x0;
+    uint64_t preceding_indirect_branch_pc = 0x0;    
     uint64_t all_total = 0;
+    uint64_t all_total_prefix = 0;
     uint64_t running_total = 0;
+    BTB btb_all;
+    BTB btb_cond;
 
     try {
-        processCommandLine(argc, argv, trace, verbose, only_taken, only_dynamic, skip_non_user);
+        processCommandLine(argc, argv, trace, verbose, only_taken, only_dynamic, skip_non_user, btb_index);
     }
     catch(const trace_tools::CommandLineParser::EarlyExitException& e) {
         std::cerr << e.what() << std::endl;
@@ -89,10 +158,16 @@ int main(int argc, char** argv) {
 
         if ((branch_info.taken == 0) && !is_taken) {
             branch_info.not_taken_prefix++;
+            all_total_prefix++;
             //continue;
         }
-        
+        else {
+            btb_all.access(branch.getPC());
+            if (!branch.isIndirect()) btb_cond.access(branch.getPC()); 
+        }
+
         if (((branch_info.taken + branch_info.not_taken) > 0) && (branch_info.previous_taken != is_taken)) {
+
             branch_info.direction_changes++;
 
             if (is_taken) {
@@ -118,31 +193,53 @@ int main(int argc, char** argv) {
                 branch_info.max_sequential_not_taken = (branch_info.max_sequential_not_taken > branch_info.sequential_not_taken) ? branch_info.max_sequential_not_taken : branch_info.sequential_not_taken;
             }
         }
+
         branch_info.taken += is_taken;
         branch_info.not_taken += !is_taken;
         branch_info.previous_taken = is_taken;
         //if (branch_info.not_taken == 0) branch_info.initial_consecutive_takens += is_taken;
         branch_info.targets[branch.getTargetPC()] += is_taken;
         branch_info.indirect = branch.isIndirect();
-        branch_info.repeated = (branch.getPC()==preceding_branch_pc);
-        preceding_branch_pc  = branch.getPC();
+        branch_info.repeated |= (branch.getPC()==preceding_branch_pc);
 
-        Direction new_dir = Direction::FORWARD;
-        if(branch.isCall()) {
-            new_dir = Direction::CALL;
-        }
-        else if(branch.isReturn()) {
-            new_dir = Direction::RETURN;
-        }
-        else if(branch.isBackwards()) {
-            new_dir = Direction::BACKWARD;
-        }
+        branch_info.precedents[preceding_branch_pc]++;
+        branch_info.pre_precedents[pre_preceding_branch_pc]++;
+        branch_info.taken_precedents[preceding_taken_branch_pc]++;
+        branch_info.indirect_precedents[preceding_indirect_branch_pc]++;
+
+        pre_preceding_branch_pc = preceding_branch_pc;
+        preceding_branch_pc  = branch.getPC();
+        if (is_taken) preceding_taken_branch_pc = branch.getPC();
+        if (branch.isIndirect()) preceding_indirect_branch_pc = branch.getPC();
+
+        Direction new_dir = (branch.isBackwards()) ? Direction::BACKWARD : Direction::FORWARD;
         if(branch_info.direction == Direction::INVALID) {
             branch_info.direction = new_dir;
         }
         else if(branch_info.direction != new_dir) {
             stf_assert(branch_info.indirect, "Only indirect branches can have multiple directions");
             branch_info.direction = Direction::MULTIPLE;
+        }
+
+        BranchType new_type = BranchType::CONDITIONAL;
+        if(branch.isCall()) {
+            new_type = (branch_info.targets.size()>1) ? BranchType::M_CALL : BranchType::CALL;
+        }
+        else if(branch.isReturn()) {
+            new_type = (branch_info.targets.size()>1) ? BranchType::M_RET : BranchType::RETURN;
+        }
+        else if(branch.isIndirect()) {
+            new_type = (branch_info.targets.size()>1) ? BranchType::M_JUMP : BranchType::JUMP;
+        }
+        
+        if( (branch_info.type == BranchType::INVALID)
+            || (branch_info.type == BranchType::JUMP)
+            || (branch_info.type == BranchType::CALL)
+            || (branch_info.type == BranchType::RETURN)) {
+            branch_info.type = new_type;
+        }
+        else if(branch_info.type != new_type) {
+            branch_info.type = BranchType::ERROR;
         }
         all_total++;
     }
@@ -158,19 +255,24 @@ int main(int argc, char** argv) {
     });
     //test = summary[0].second.not_taken;
     //std::cout << "Test after sort: " << test << std::endl;
-    std::cout << "Total Instances: " << all_total << std::endl;
-    uint64_t limit = int(0.98*(double)all_total);
+    std::cout << "Total Instances: " << all_total << " Total prefix NTs: " << all_total_prefix << std::endl;
+    uint64_t limit = int(0.98*(double)(all_total-all_total_prefix));
     bool limit_reached = false;
 
     static constexpr int COLUMN_WIDTH = 17;
     stf::print_utils::printLeft("Rank", 6);
     stf::print_utils::printLeft("Instr PC", COLUMN_WIDTH);
+    stf::print_utils::printLeft("Type/Behav", 12);
+    //stf::print_utils::printLeft("Behavior", 12);
+    stf::print_utils::printLeft("PrePrecedes", 12);    
+    stf::print_utils::printLeft("Precedents", 12);
+    stf::print_utils::printLeft("TknPrecedes", 12);
+    stf::print_utils::printLeft("IndPrecedes", 12);
+    stf::print_utils::printLeft("Repeated", 12);
+    stf::print_utils::printLeft("Targets", 12);
+    stf::print_utils::printLeft("MaxDistance", 12);
+    stf::print_utils::printLeft("Direction", 12);
     stf::print_utils::printLeft("Total Instances", COLUMN_WIDTH);
-    stf::print_utils::printLeft("Behavior", COLUMN_WIDTH);
-    stf::print_utils::printLeft("Direction", COLUMN_WIDTH);
-    stf::print_utils::printLeft("Indirect", COLUMN_WIDTH);
-    stf::print_utils::printLeft("Repeated", COLUMN_WIDTH);    
-    stf::print_utils::printLeft("# Targets", COLUMN_WIDTH);
     stf::print_utils::printLeft("NT Prefix", COLUMN_WIDTH); 
     stf::print_utils::printLeft("Takens", COLUMN_WIDTH);
     stf::print_utils::printLeft("Not Takens", COLUMN_WIDTH);
@@ -190,7 +292,8 @@ int main(int argc, char** argv) {
         const auto& branch_info = branch.second;
         const auto taken = branch_info.taken;
         const auto not_taken = branch_info.not_taken;
-
+        const auto not_taken_prefix = branch_info.not_taken_prefix;
+        const auto not_taken_after_prefix = not_taken - not_taken_prefix;
 
         if(only_dynamic && !(taken && not_taken)) {
             continue;
@@ -214,52 +317,103 @@ int main(int argc, char** argv) {
         stf::print_utils::printDecLeft(++rank, 6);
         stf::print_utils::printHex(pc);
         stf::print_utils::printSpaces(4);
-        stf::print_utils::printDecLeft(total, COLUMN_WIDTH);
 
-        if(taken && !not_taken) {
-            stf::print_utils::printLeft("AT", COLUMN_WIDTH);
+        switch(branch_info.type) {
+            case BranchType::CONDITIONAL:
+                if (taken && !not_taken) stf::print_utils::printLeft("  CAT", 12);
+                else if (taken && not_taken_after_prefix) {
+                    if (branch_info.repeated && branch_info.direction==Direction::BACKWARD) 
+                        stf::print_utils::printLeft("C1BBL", 12);
+                    else  stf::print_utils::printLeft(" CDYN", 12);
+                }
+                else stf::print_utils::printLeft(" COND", 12);
+                break;
+            case BranchType::CALL:
+                stf::print_utils::printLeft(" Call", 12);
+                break;
+            case BranchType::M_CALL:
+                stf::print_utils::printLeft("M_Call", 12);
+                break;
+            case BranchType::RETURN:
+                stf::print_utils::printLeft(" Ret", 12);
+                break;
+            case BranchType::M_RET:
+                stf::print_utils::printLeft("M_Ret", 12);
+                break;
+            case BranchType::JUMP:
+                stf::print_utils::printLeft(" Jump", 12);
+                break;
+            case BranchType::M_JUMP:
+                stf::print_utils::printLeft("M_Jump", 12);
+                break;
+            case BranchType::ERROR:
+                stf::print_utils::printLeft("ERROR", 12);
+                break;
+            case BranchType::INVALID:
+                stf_throw("Invalid branch direction for pc " << std::hex << pc);
+        };
+        
+        // if(taken && !not_taken) {
+        //     if (branch_info.indirect)
+        //         stf::print_utils::printLeft("at", 12);
+        //     else
+        //         stf::print_utils::printLeft("AT", 12);
+        // }
+        // else if(!taken && not_taken) {
+        //     stf::print_utils::printLeft("NT", 12);
+        // }
+        // else if((taken==1) && not_taken) {
+        //     stf::print_utils::printLeft("1_T", 12);
+        // }
+        // else if(taken && (not_taken_after_prefix==1)) {
+        //     stf::print_utils::printLeft("1_NT", 12);
+        // }
+        // else if(taken && not_taken_after_prefix) {
+        //     stf::print_utils::printLeft("DYN", 12);
+        // }
+
+        stf::print_utils::printDecLeft(branch_info.pre_precedents.size(), 12);
+        stf::print_utils::printDecLeft(branch_info.precedents.size(), 12);
+        stf::print_utils::printDecLeft(branch_info.taken_precedents.size(), 12);
+        stf::print_utils::printDecLeft(branch_info.indirect_precedents.size(), 12);
+        if(branch_info.repeated) {
+            if (branch_info.direction==Direction::FORWARD) stf::print_utils::printLeft("Y->", 12);
+            else if (branch_info.direction==Direction::BACKWARD) stf::print_utils::printLeft("<-Y", 12);
+            else stf::print_utils::printLeft("<Y>", 12);
         }
-        else if(!taken && not_taken) {
-            stf::print_utils::printLeft("NT", COLUMN_WIDTH);
+        else {
+            stf::print_utils::printLeft(" n", 12);
         }
-        else if(taken && not_taken) {
-            stf::print_utils::printLeft("DYN", COLUMN_WIDTH);
+
+        stf::print_utils::printDecLeft(branch_info.targets.size(), 12);
+
+        uint64_t max_distance = 0;
+        for (const auto& [target,count] : branch_info.targets) {
+            uint64_t distance = std::abs((long long)pc - (long long)target);
+            max_distance = (distance > max_distance) ? distance : max_distance;
         }
+        if (!branch_info.indirect) {
+            stf::print_utils::printSpaces(2);
+            stf::print_utils::printHex(max_distance,6);
+        }
+        else stf::print_utils::printHex(max_distance,8);
+        stf::print_utils::printSpaces(2);
 
         switch(branch_info.direction) {
             case Direction::FORWARD:
-                stf::print_utils::printLeft("Fw->", COLUMN_WIDTH);
+                stf::print_utils::printLeft("Fw->", 12);
                 break;
             case Direction::BACKWARD:
-                stf::print_utils::printLeft("<-Bk", COLUMN_WIDTH);
-                break;
-            case Direction::CALL:
-                stf::print_utils::printLeft("CALL", COLUMN_WIDTH);
-                break;
-            case Direction::RETURN:
-                stf::print_utils::printLeft("RETURN", COLUMN_WIDTH);
+                stf::print_utils::printLeft("<-Bk", 12);
                 break;
             case Direction::MULTIPLE:
-                stf::print_utils::printLeft("MULTIPLE", COLUMN_WIDTH);
+                stf::print_utils::printLeft("MULT", 12);
                 break;
             case Direction::INVALID:
                 stf_throw("Invalid branch direction for pc " << std::hex << pc);
         };
 
-        if(branch_info.indirect) {
-            stf::print_utils::printLeft('Y', COLUMN_WIDTH);
-        }
-        else {
-            stf::print_utils::printLeft(" n", COLUMN_WIDTH);
-        }
-        if(branch_info.repeated) {
-            stf::print_utils::printLeft('Y', COLUMN_WIDTH);
-        }
-        else {
-            stf::print_utils::printLeft(" n", COLUMN_WIDTH);
-        }
-
-        stf::print_utils::printDecLeft(branch_info.targets.size(), COLUMN_WIDTH);
+        stf::print_utils::printDecLeft(total, COLUMN_WIDTH);
         stf::print_utils::printDecLeft(branch_info.not_taken_prefix, COLUMN_WIDTH);
         stf::print_utils::printDecLeft(taken, COLUMN_WIDTH);
         stf::print_utils::printDecLeft(not_taken, COLUMN_WIDTH);
@@ -295,9 +449,43 @@ int main(int argc, char** argv) {
                 std::cout << std::endl;
             }
         }
-
         std::cout << std::endl;
     }
-
+    
+    if(btb_index) {
+        std::cout << std::endl;
+        stf::print_utils::printLeft("Index", 6);
+        stf::print_utils::printLeft("All Allocs", COLUMN_WIDTH);
+        stf::print_utils::printSpaces(6);
+        stf::print_utils::printLeft("Unique PCs", COLUMN_WIDTH);
+        stf::print_utils::printSpaces(6);
+        stf::print_utils::printLeft("All Hits", COLUMN_WIDTH);
+        stf::print_utils::printSpaces(COLUMN_WIDTH);
+        stf::print_utils::printLeft("Cond Allocs", COLUMN_WIDTH);
+        stf::print_utils::printSpaces(6);
+        stf::print_utils::printLeft("Uniq Cond PCs", COLUMN_WIDTH);
+        stf::print_utils::printSpaces(6);
+        stf::print_utils::printLeft("Cond Hits", COLUMN_WIDTH);
+        std::cout << std::endl;
+        for (uint64_t i=0; i<1024; i++) {
+            stf::print_utils::printDecLeft(i, 6);
+            stf::print_utils::printDecLeft(btb_all.getAllocations(i), COLUMN_WIDTH);
+            if (btb_all.getAllocations(i)!=0) {
+                stf::print_utils::printDecLeft(btb_all.getNumUniquePCs(i), COLUMN_WIDTH);
+                if (btb_all.getAllocations(i)>btb_all.getHits(i)) stf::print_utils::printLeft(">", 6);
+                else stf::print_utils::printSpaces(6);
+                stf::print_utils::printDecLeft(btb_all.getHits(i), COLUMN_WIDTH);
+                stf::print_utils::printSpaces(COLUMN_WIDTH);
+                stf::print_utils::printDecLeft(btb_cond.getAllocations(i), COLUMN_WIDTH);
+                if (btb_cond.getAllocations(i)!=0) {
+                    stf::print_utils::printDecLeft(btb_cond.getNumUniquePCs(i), COLUMN_WIDTH);
+                    if (btb_cond.getAllocations(i)>btb_cond.getHits(i)) stf::print_utils::printLeft(">", 6);
+                    else stf::print_utils::printSpaces(6);
+                    stf::print_utils::printDecLeft(btb_cond.getHits(i), COLUMN_WIDTH);
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
     return 0;
 }
